71f4df24e2b5dd1bc50dc488c03bb44936629fa7
Harsh Shrivastava <62792682+hmarshmello@users.noreply.github.com>
2023-02-04 19:16:24 +0530

    hehe

--- Viz/initViz.js
+++ Viz/initViz.js
@@ -1,6 +1,5 @@
 const containerDiv = document.getElementById("vizContainer");
 const URL = "https://public.tableau.com/views/JusttheData-WorldHappiness_16526926875300/Family?:language=en-GB&:display_count=n&:origin=viz_share_link";
-const fixedURL = "https://public.tableau.com/views/GlobalCO2Emissions_16526140229080/FixedSize";
 let viz;
 
 const options = {
